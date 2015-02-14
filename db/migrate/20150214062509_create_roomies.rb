391a0dd9a0881917bbcfcd3b2cede1264d8e2ec1
rjmccluskey <rjmccluskey@gmail.com>
2015-02-13 23:31:00 -0800

    migrate roomies

--- db/migrate/20150214062509_create_roomies.rb
+++ db/migrate/20150214062509_create_roomies.rb
@@ -1,6 +1,14 @@
 class CreateRoomies < ActiveRecord::Migration
   def change
     create_table :roomies do |t|
+      t.string :nickname
+      t.string :first_name
+      t.string :last_name
+      t.string :display_name
+      t.string :email
+      t.string :phone
+      t.string :profile_picture_url
+      t.string :venmo_id
 
       t.timestamps null: false
     end
