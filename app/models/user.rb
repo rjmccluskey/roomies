f5439d34131038e898092ffd350c8e80bb608493
rjmccluskey <rjmccluskey@gmail.com>
2015-02-14 08:11:38 -0800

    validate user columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates_presence_of :nickname, :first_name, :last_name, :display_name, :email, :phone, :profile_picture_url, :venmo_id
+  validates_uniqueness_of :email, :venmo_id, case_sensitive: false
+  validates_length_of :nickname, in: 2..20
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it {should have_db_column(:nickname)}
-  it {should have_db_column(:first_name)}
-  it {should have_db_column(:last_name)}
-  it {should have_db_column(:display_name)}
-  it {should have_db_column(:email)}
-  it {should have_db_column(:phone)}
-  it {should have_db_column(:profile_picture_url)}
-  it {should have_db_column(:venmo_id)}
+  it {should have_db_column :nickname}
+  it {should have_db_column :first_name}
+  it {should have_db_column :last_name}
+  it {should have_db_column :display_name}
+  it {should have_db_column :email}
+  it {should have_db_column :phone}
+  it {should have_db_column :profile_picture_url}
+  it {should have_db_column :venmo_id}
+
+  it {should validate_presence_of :nickname}
+  it {should validate_presence_of :first_name}
+  it {should validate_presence_of :last_name}
+  it {should validate_presence_of :display_name}
+  it {should validate_presence_of :email}
+  it {should validate_presence_of :phone}
+  it {should validate_presence_of :profile_picture_url}
+  it {should validate_presence_of :venmo_id}
+
+  it {should validate_uniqueness_of(:email).case_insensitive}
+  it {should validate_uniqueness_of(:venmo_id).case_insensitive}
+
+  it {should ensure_length_of(:nickname).is_at_least(2).is_at_most(20)}
 end
