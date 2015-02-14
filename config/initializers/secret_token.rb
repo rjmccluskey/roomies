454f909ec8fa9c6363dba7e50593a4700714aef6
rjmccluskey <rjmccluskey@gmail.com>
2015-02-13 20:15:53 -0800

    add rails-api skeleton configured with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Roomies::Application.config.secret_token = '23390364d5ff5865f2cee3fc3c62ebb8762e1175a061486399394b9ac959eaed238a77770bff071bad56ef5ba8d885bda59b5b76a340439d0327637405d88e2d'
