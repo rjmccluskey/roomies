1354a12ec7b3dd2ef749f355d329c249ec5d70d8
rjmccluskey <rjmccluskey@gmail.com>
2015-02-18 12:28:11 -0800

    add validations for houses

--- server/app/models/house.rb
+++ server/app/models/house.rb
@@ -1,3 +1,8 @@
 class House < ActiveRecord::Base
   has_secure_password
+
+  validates_presence_of :name, :password_digest
+  validates_uniqueness_of :name
+  validates_length_of :name, in: 3..20
+  validates_length_of :password, in: 8..20
 end
--- server/spec/models/house_spec.rb
+++ server/spec/models/house_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe House, :type => :model do
   it {should have_db_column :name}
   it {should have_db_column :password_digest}
+
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :password_digest}
+
+  it {should validate_uniqueness_of :name}
+
+  it {should ensure_length_of(:name).is_at_least(3).is_at_most(20)}
+  it {should ensure_length_of(:password).is_at_least(8).is_at_most(20)}
+
   it {should have_secure_password }
 end
