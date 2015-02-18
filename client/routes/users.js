e8f7cb058b51777d89a517f25bee4a868a91e55d
rjmccluskey <rjmccluskey@gmail.com>
2015-02-17 21:17:32 -0800

    redirect to home from venmo_oauth if no code is provided

--- client/routes/users.js
+++ client/routes/users.js
@@ -3,14 +3,21 @@ var router = express.Router();
 var rest = require('restler')
 
 router.get('/venmo_oauth', function(req, res) {
-  rest
-    .post('http://localhost:3000/users',
-      { data: {'code': req.query.code} })
-    .on('complete', function(data) {
-      req.session.user_id = data.user_id
-      req.session.access_token = data.access_token
-      res.redirect('/')
-    });
+  var code = req.query.code;
+
+  if (code) {
+    rest
+      .post('http://localhost:3000/users',
+        { data: {'code': code} })
+      .on('complete', function(data) {
+        req.session.user_id = data.user_id;
+        req.session.access_token = data.access_token;
+        res.redirect('/');
+      });
+  }
+  else {
+    res.redirect('/');
+  }
 });
 
 router.get('/logout', function(req, res) {
