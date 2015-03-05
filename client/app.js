3211f974701e5386081ed1a700ed7045a0659ef8
rjmccluskey <rjmccluskey@gmail.com>
2015-03-04 18:29:58 -0800

    oauth login loads page generated with react

--- client/app.js
+++ client/app.js
@@ -17,8 +17,8 @@ var houses = require('./routes/houses');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- client/public/javascripts/main.js
+++ client/public/javascripts/main.js
@@ -1,3 +1,37 @@
 (function(){
+  var UserInfo = React.createClass({
+    loadUserFromServer: function() {
+      $.ajax({
+        url: "/users",
+        dataType: 'json',
+        success: function(data) {
+          console.log(this);
+          this.setState(data);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+    getInitialState: function() {
+      return {user: {}};
+    },
+    componentDidMount: function() {
+      this.loadUserFromServer();
+    },
+    render: function() {
+      var user = this.state.user;
+      return (
+        <div className="userInfo container">
+          <img src={user.profile_picture_url} />
+          <h1>{user.first_name}</h1>
+        </div>
+      );
+    }
+  });
 
+  React.render(
+    <UserInfo />,
+    document.getElementById('content')
+  );
 }());
