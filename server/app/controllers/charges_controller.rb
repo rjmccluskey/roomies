6af26eea89452d1354e3ef740f4302831bb1ae38
rjmccluskey <rjmccluskey@gmail.com>
2015-03-24 00:24:59 -0700

    test venmo webhook

--- server/app/controllers/charges_controller.rb
+++ server/app/controllers/charges_controller.rb
@@ -4,12 +4,14 @@ class ChargesController < ApplicationController
   end
 
   def update_status
-
+    p "============================"
+    p permit_params[:data]
+    p "============================"
   end
 
   protected
 
   def permit_params
-    params.permit(:token, :venmo_challenge)
+    params.permit(:token, :venmo_challenge, :date_created, :type, :data)
   end
 end
