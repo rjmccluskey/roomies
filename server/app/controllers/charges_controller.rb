0bc10c87e3b78112b34f0cbac0e067f4b8dcbb74
rjmccluskey <rjmccluskey@gmail.com>
2015-03-24 12:52:14 -0700

    use ActiveSupport::JSON decode to read json in webhook

--- server/app/controllers/charges_controller.rb
+++ server/app/controllers/charges_controller.rb
@@ -1,19 +1,17 @@
 class ChargesController < ApplicationController
+  include
   def verify_webhook
     render json: permit_params[:venmo_challenge]
   end
 
   def update_status
-    p "============================"
-    p JSON.parse(permit_params[:data])
-    p "============================"
-    p permit_params[:type]
-    p "============================"
+    venmo_webhook = ActiveSupport::JSON.decode(request.body.string)
+    p venmo_webhook
   end
 
   protected
 
   def permit_params
-    params.permit(:token, :venmo_challenge, :date_created, :type, :data)
+    params.permit(:token)
   end
 end
