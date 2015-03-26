4aa5c7aa2b0398e582dafcee90149f3342186751
rjmccluskey <rjmccluskey@gmail.com>
2015-03-25 23:20:20 -0700

    give sections a minimum height for narrow screens

--- client/public/javascripts/splash.js
+++ client/public/javascripts/splash.js
@@ -1,9 +1,18 @@
 (function(){
-  $('#header').css('width', $(window).width());
-  $('#header').css('height', $(window).height());
+  fitToWindow($('section'));
 
   $(window).resize(function() {
-    $('#header').css('width', $(window).width());
-    $('#header').css('height', $(window).height());
+    fitToWindow($('section'));
   });
+
+  function fitToWindow(node) {
+    var windowHeight = $(window).height();
+    var minHeight = node.css('min-height');
+    var height = windowHeight;
+    if (windowHeight < minHeight) {
+      height = minHeight;
+    };
+    node.css('width', $(window).width());
+    node.css('height', height);
+  };
 })();
